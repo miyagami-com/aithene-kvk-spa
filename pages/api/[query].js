789e3b70eddef68a9e2c481a23ef5244644c2cc6
nikitaSazhinov <nikitasazhinovpro@gmail.com>
2021-10-15 15:42:39 +0200

    Api returns query results

--- pages/api/[query].js
+++ pages/api/[query].js
@@ -1,32 +1,88 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+//const express = require('express');
+// const browserObject = require('./browser');
+const puppeteer = require('puppeteer');
+// const scraperController = require('./pageController');
+
+let browserInstance = startBrowser();
+
+async function startBrowser(){
+    let browser;
+    try {
+        console.log("Opening the browser......");
+        browser = await puppeteer.launch({
+            headless: true,
+            args: ["--disable-setuid-sandbox"],
+            'ignoreHTTPSErrors': true
+        });
+    } catch (err) {
+        console.log("Could not create a browser instance => : ", err);
+    }
+    return browser;
+}
+
+async function scrapeAll(browserInstance, query){
+    let browser;
+    try{
+        browser = await browserInstance;
+        console.log("Scraped all");
+        return await pageScraper(browser, query);
+    }
+    catch(err){
+        console.log("Could not resolve the browser instance => ", err);
+    }
+}
+
+async function pageScraper(browser, query){
+    const url = `https://www.kvk.nl/zoeken/handelsregister/?handelsnaam=${query}`
+    let page = await browser.newPage();
+
+    await page.setViewport({width: 1366, height: 1400})
+    console.log(`Navigating to ${url}...`);
+    // Navigate to the selected page
+    await page.goto(url);
+    // Wait for the required DOM to be rendered
+    await page.waitForSelector('#js-search-results > div > ul.results');
+    // Get the link to all the required books
+    let urls = await page.$$eval('#js-search-results > div > ul.results > li', items => {
+        let data = [];
+
+        console.log('items', items)
+
+        names = items.map((el) => el.querySelector('div.more-search-info > p').textContent)
+        console.log('names', names)
+        kvks = items.map((el) => el.querySelector('div.content > ul > li:nth-child(1)').textContent)
+        console.log('kvks', kvks)
+        links = items.map((el) => el.querySelector('div.handelsnaamHeaderWrapper > h3 > a').href)
+        console.log('links', links)
+
+        for (let i = 0; i < items.length; i++) {
+            data[i] = {
+                name: names[i],
+                kvk: kvks[i].slice(4,-1),
+                href: [links[i]]
+            }
+        }
+
+        return data;
+    });
+    return urls;
+}
+
+
 export default function handler(req, res) {
-  const dummyData = {
-    query: req.query.query,
-    name: "Hello",
-    items: [
-      {
-        name: "Miyagmi BV",
-        kvk: "432442432",
-        href: "http://miyagami.com"
-      },
-      {
-        name: "Miyagmi BV",
-        kvk: "432442432",
-        href: "http://miyagami.com"
-      },
-      {
-        name: "Miyagmi BV",
-        kvk: "432442432",
-        href: "http://miyagami.com"
-      },
-      {
-        name: "Miyagmi BV",
-        kvk: "432442432",
-        href: "http://miyagami.com"
-      }
-    ]
+  let query = req.query.query;
+
+  try {
+    scrapeAll(browserInstance, query).then((val) => {
+      //console.log(val)
+      res.send((val))
+    });
+  } catch (e) {
+    res.send(e);
   }
 
-  res.status(200).json(dummyData)
+
+  //res.status(200).json({ name: 'John Doe' })
 }
