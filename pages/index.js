243b3856545ea4a83a38c975d4742e04cb42d87b
dale wesdorp <dalewesdorp@gmail.com>
2021-10-15 17:25:56 +0200

    Added .idea to gitignore

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import axios from 'axios';
 
-import {Button, Layout, message, PageHeader, Upload} from 'antd';
+import {Button, Input, Layout, message, PageHeader, Upload, Typography} from 'antd';
 import {CloudUploadOutlined, DownloadOutlined} from '@ant-design/icons';
 import {TableComponent} from "../components/Table";
 import {useEffect, useState} from "react";
 import * as XLSX from 'xlsx';
 
 const {Header, Content, Footer} = Layout;
+const {Title} = Typography;
 
 export default function Home() {
     const [query, setQuery] = useState('');
@@ -40,12 +41,12 @@ export default function Home() {
             // evt = on_file_select event
             /* Parse data */
             const bstr = evt.target.result;
-            const wb = XLSX.read(bstr, { type: "binary" });
+            const wb = XLSX.read(bstr, {type: "binary"});
             /* Get first worksheet */
             const wsname = wb.SheetNames[0];
             const ws = wb.Sheets[wsname];
             /* Convert array of arrays */
-            const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+            const data = XLSX.utils.sheet_to_csv(ws, {header: 1});
             /* Update state */
             setUpload(JSON.parse(convertToJson(data))); // shows data in json format
             fetchData(JSON.parse(convertToJson(data))[0].name)
@@ -76,6 +77,12 @@ export default function Home() {
         return JSON.stringify(result); //JSON
     }
 
+    const handleEnter = (e) => {
+        if (e.key === "Enter") {
+            return fetchData(query);
+        }
+    };
+
     const uploadProps = {
         name: 'file',
         accept: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
@@ -99,6 +106,7 @@ export default function Home() {
         },
     };
 
+
     const incrementStep = () => {
         const nextStep = currentStep + 1;
         setCurrentStep(nextStep)
@@ -111,31 +119,13 @@ export default function Home() {
 
     return (
         <Layout className="layout" theme="light">
-            <Header theme="light">
-                <div className="logo"/>
-            </Header>
-            <Content style={{padding: '40px'}}>
-                <PageHeader
-                    className="site-page-header"
-                    title={`${currentStep ? `${currentStep} / ${upload?.length}` : ''} ${data?.name || "Search query"}`}
-                    subTitle={`${data?.items?.length || ''} ${data?.items?.length ? 'Results' : ''}`}
-                />
-                <div style={{display: 'flex', flexDirection: 'row'}}>
-                    <Button
-                        loading={loading}
-                        disabled={loading}
-                        onClick={() => incrementStep()}
-                    >
-                       Next step
-                    </Button>
-                    <Button
-                        type="primary"
-                        icon={<DownloadOutlined/>}
-                        disabled={!download}
-                        loading={downloading}
-                    >
-                        Download
-                    </Button>
+            <Header style={{background: 'white', display: 'flex', flexDirection: 'row'}}>
+                <Title className="logo" level={3}>Aithena KVK Scraper</Title>
+                <Input placeholder="Search Company" style={{
+                    width: 400,
+                    margin: 'auto'
+                }} onChange={(e) => setQuery(e.target.value)} onKeyDown={handleEnter}/>
+                <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
                     <Upload {...uploadProps}>
                         <Button
                             icon={<CloudUploadOutlined/>}
@@ -146,7 +136,29 @@ export default function Home() {
                             Upload
                         </Button>
                     </Upload>
+                    <Button
+                        loading={loading}
+                        disabled={!upload}
+                        onClick={() => incrementStep()}
+                    >
+                        Next step
+                    </Button>
+                    <Button
+                        type="primary"
+                        icon={<DownloadOutlined/>}
+                        disabled={!download}
+                        loading={downloading}
+                    >
+                        Download
+                    </Button>
                 </div>
+            </Header>
+            <Content style={{padding: '10px 30px'}}>
+                <PageHeader
+                    className="site-page-header"
+                    title={`${currentStep ? `${currentStep} / ${upload?.length}` : ''} ${data?.name || "Search query"}`}
+                    subTitle={`${data?.items?.length || ''} ${data?.items?.length ? 'Results' : ''}`}
+                />
                 <div className="site-layout-content">
                     <TableComponent data={data?.items} setData={setData}/>
                 </div>
