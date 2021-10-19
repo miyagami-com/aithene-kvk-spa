312affe3cc3ae5fd0a018924386ffc78ad779667
dale wesdorp <dalewesdorp@gmail.com>
2021-10-19 17:14:23 +0200

    Added an async try catch to the API route

--- pages/api/[query].js
+++ pages/api/[query].js
@@ -2,9 +2,7 @@
 
 const puppeteer = require('puppeteer');
 
-let browserInstance = startBrowser();
-
-async function startBrowser(){
+async function startBrowser() {
     let browser;
     try {
         browser = await puppeteer.launch({
@@ -18,37 +16,40 @@ async function startBrowser(){
     return browser;
 }
 
-async function scrapeAll(browserInstance, query){
+async function scrapeAll(browserInstance, query) {
     let browser;
-    try{
+    try {
         browser = await browserInstance;
         return await pageScraper(browser, query);
-    }
-    catch(err) {
+    } catch (err) {
         return (err)
     }
 }
 
-async function pageScraper(browser, query){
+async function pageScraper(browser, query) {
     const url = `https://www.kvk.nl/zoeken/handelsregister/?handelsnaam=${encodeURIComponent(query)}`
-    let page = await browser.newPage();
+    let page = await browser.newPage().catch(error => {
+        return (error);
+    });
 
-    await page.setViewport({width: 1366, height: 1400})
+    await page.setViewport({width: 1366, height: 1400}).catch(error => {
+        return (error);
+    });
     // Navigate to the selected page
-    await page.goto(url);
+    await page.goto(url).catch(error => {
+        return (error);
+    });
     // Wait for the required DOM to be rendered
 
     await page.waitForSelector('#js-search-results > div > ul.results').catch(error => {
-      return (error);
+        return (error);
     });
     // Get the link to all the required books
     return await page.$$eval('#js-search-results > div > ul.results > li', items => {
         let data = [];
-
         const names = items.map((el) => el.querySelector('div.more-search-info > p').textContent)
         const kvks = items.map((el) => el.querySelector('div.content > ul > li:nth-child(1)').textContent)
         const links = items.map((el) => el.querySelector('div.handelsnaamHeaderWrapper > h3 > a').href)
-
         for (let i = 0; i < items.length; i++) {
             data[i] = {
                 name: names[i],
@@ -64,12 +65,14 @@ async function pageScraper(browser, query){
 
 
 export default async function handler(req, res) {
-  let query = req.query.query;
-  try {
-    await scrapeAll(browserInstance, query).then((val) => {
-      res.status(200).send(val)
-    });
-  } catch (e) {
-    res.status(400).send(e);
-  }
+    let query = req.query.query;
+    let browserInstance = startBrowser();
+
+    try {
+        await scrapeAll(browserInstance, query).then((val) => {
+            res.status(200).send(val)
+        });
+    } catch (e) {
+        res.status(400).send(e);
+    }
 }
